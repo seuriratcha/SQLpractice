d8f001cad2dbfad59b344dc8500bedb89ae2c853
seuriratcha <155952980+seuriratcha@users.noreply.github.com>
2024-02-01 19:09:47 +0900

    Update case_when_1.sql

--- case_when_1.sql
+++ case_when_1.sql
@@ -1,16 +1,7 @@
 -- "Type of Triangle"
 -- https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true
 
-/*
-Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. Output one of the following statements for each record in the table:
-  Equilateral: It's a triangle with  sides of equal length.
-  Isosceles: It's a triangle with  sides of equal length.
-  Scalene: It's a triangle with  sides of differing lengths.
-  Not A Triangle: The given values of A, B, and C don't form a triangle.
-*/
-
---- Query
-
+/* Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. */
 SELECT CASE 
   WHEN A = B AND B = C THEN 'Equilateral'
   WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
