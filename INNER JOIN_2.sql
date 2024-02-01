a2f35f4aa5bfa1a2d0a56e2f898c0ee4cf194888
seuriratcha <155952980+seuriratcha@users.noreply.github.com>
2024-02-01 19:10:42 +0900

    Update INNER JOIN_2.sql

--- INNER JOIN_2.sql
+++ INNER JOIN_2.sql
@@ -1,6 +1,7 @@
 -- "Asian Population"
 -- https://www.hackerrank.com/challenges/asian-population/problem
 
+/* query the sum of the populations of all cities where the CONTINENT is 'Asia' */
 SELECT SUM(city.population)
 FROM city
     INNER JOIN country on city.CountryCode = country.Code
