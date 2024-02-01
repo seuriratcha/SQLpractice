fb980ca065881e3ff4e8fc3af95941f4086a806f
seuriratcha <155952980+seuriratcha@users.noreply.github.com>
2024-02-01 19:20:45 +0900

    Update INNER JOIN_2.sql

--- INNER JOIN_2.sql
+++ INNER JOIN_2.sql
@@ -6,3 +6,16 @@ SELECT SUM(city.population)
 FROM city
     INNER JOIN country on city.CountryCode = country.Code
 WHERE country.continent = 'Asia'
+
+----------------------------
+
+-- "Average Population of Each Continent"
+-- https://www.hackerrank.com/challenges/average-population-of-each-continent/problem
+    
+/* query the names of all the continents and their respective average city populations rounded down to the nearest integer.*/
+SELECT country.Continent, FLOOR(AVG(city.Population))
+FROM city
+    INNER JOIN country ON city.CountryCode = country.Code
+GROUP BY country.Continent
+
+-- 소수점 버림 = FLOOR
