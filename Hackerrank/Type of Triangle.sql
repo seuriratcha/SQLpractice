47d9c92ff7b6f97602671803a7b4137244e7f6f6
seuriratcha <155952980+seuriratcha@users.noreply.github.com>
2024-02-01 15:09:40 +0900

    Update Type of Triangle.sql

--- Hackerrank/Type of Triangle.sql
+++ Hackerrank/Type of Triangle.sql
@@ -9,11 +9,11 @@ Write a query identifying the type of each record in the TRIANGLES table using i
 --- Query
 
 SELECT CASE 
-WHEN A = B AND B = C THEN 'Equilateral'
-WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
-WHEN A = B OR B = C OR A = C THEN 'Isosceles'
-ELSE 'Scalene'
-END
+  WHEN A = B AND B = C THEN 'Equilateral'
+  WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+  WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+  ELSE 'Scalene'
+  END
 FROM triangles
 
 -- WHEN 절은 순서 중요함, 위에서부터 실행되기 때문에 중복 조건 있으면 원하는 값 잘못 출력될 수 있음
